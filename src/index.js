2e05e6b7d219f5b2b169a197bd28c3fef02af228
lawrenceal <lawrenceal@163.com>
2017-07-13 11:13:17 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>hello redux</h1>, document.getElementById('root'));
 
