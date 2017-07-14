291f57860c2a937022122914628e22a26a1d63ca
lawrenceal <lawrenceal@163.com>
2017-07-14 16:15:01 +0800

    redux todolist

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import VisibleTodoList from './VisibleTodoList';
 import Filter from './Filter';
+import AddTodo from './AddTodo';
 import '../style/todoList.css';
 
 const TodoApp = () => {
     return (<div className="todo-container">
+        <AddTodo/>
         <Filter/>
         <VisibleTodoList/>
     </div>);
